28a32b0334cc7e70be6b42c0162b5eb1dd5aab7c
memewo8819 <sumi881917103688@gmail.com>
2020-07-29 17:00:21 +0900

    2020-07-29 17:00

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -90,9 +90,19 @@ class TweetsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Tweet $tweet)
     {
-        //
+        $user = auth()->user();
+        $tweets = $tweet->getEditTweet($user->id, $tweet->id);
+
+        if (!isset($tweets)) {
+            return redirect('tweets');
+        }
+
+        return view('tweets.edit', [
+            'user' => $user,
+            'tweets' => $tweets
+        ]);
     }
 
     /**
@@ -102,9 +112,17 @@ class TweetsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Tweet $tweet)
     {
-        //
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'text' => ['required', 'string', 'max:140']
+        ]);
+
+        $validator->validate();
+        $tweet->tweetUpdate($tweet->id, $data);
+
+        return redirect('tweets');
     }
 
     /**
@@ -113,8 +131,11 @@ class TweetsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Tweet $tweet)
     {
-        //
+        $user = auth()->user();
+        $tweet->tweetDestroy($user->id, $tweet->id);
+
+        return back();
     }
 }
